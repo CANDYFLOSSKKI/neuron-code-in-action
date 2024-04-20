091f77cc85f660644dc150d098507f918120780f
CANDYFLOSSKKI <77969014+CANDYFLOSSKKI@users.noreply.github.com>
2024-04-20 21:31:12 +0800

    24/4/20 fix csharp part

--- csharp-sample/basic/obj/Debug/net7.0/basic.AssemblyInfo.cs
+++ csharp-sample/basic/obj/Debug/net7.0/basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d291545a8ea5b14208a4a04b76c031b0839cf31")]
 [assembly: System.Reflection.AssemblyProductAttribute("basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
